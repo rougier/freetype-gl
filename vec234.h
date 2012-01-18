7cb1d05597762f3eaef333ed2f995d599bfd01ac
Nicolas.Rougier <Nicolas.Rougier@gmail.com>
2012-01-18 15:33:57 +0000

    Fix bug with duplicate members

--- vec234.h
+++ vec234.h
@@ -50,7 +50,7 @@ typedef union
     float rgb[3];
     float xyz[3];
 	struct { float x;   float y;     float z;     float w;      };
-	struct { float x;   float y;     float width; float height; };
+	struct { float x_;   float y_;   float width; float height; };
 	struct { float r;   float g;     float b;     float a;      };
 	struct { float red; float green; float blue;  float alpha;  };
 } vec4;
@@ -94,7 +94,7 @@ typedef union
     int rgb[3];
     int xyz[3];
 	struct { int x;   int y;     int z;     int w;      };
-	struct { int x;   int y;     int width; int height; };
+	struct { int x_;  int y_;    int width; int height; };
 	struct { int r;   int g;     int b;     int a;      };
 	struct { int red; int green; int blue;  int alpha;  };
 } ivec4;
