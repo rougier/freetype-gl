b3e52404ecabd5de117108043fe48961b21e579f
Xinchang <i+git@liux.ch>
2014-03-20 23:09:56 +0100

    Xcode project for OSX added

---
+++ osx/FreeType-GL_OSX_Demos/FreeType-GL_OSX_Demos/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FreeType-GL_OSX_Demos
+//
+//  Created by Xinchang LIU on 20/03/14.
+//  Copyright (c) 2014 Xinchang LIU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
