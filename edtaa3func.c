a4cfb9abac19a0ab62b625a9b6f856e032fe3732
Marcel Metz <mmetz@adrian-broher.net>
2016-04-08 13:21:34 +0200

    Update edtaa3func.c to latest upstream version
    Thanks to Stefan Gustavson (edtaa3func.c dev) for pointing out the fixed
    version.
    
    Fixes #110.

--- edtaa3func.c
+++ edtaa3func.c
@@ -1,33 +1,4 @@
 /*
- * Copyright 2009 Stefan Gustavson (stefan.gustavson@gmail.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  1. Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY STEFAN GUSTAVSON ''AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL STEFAN GUSTAVSON OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of Stefan Gustavson.
- *
- *
  * edtaa3()
  *
  * Sweep-and-update Euclidean distance transform of an
@@ -54,9 +25,34 @@
  *
  * Updated in 2011 to avoid a corner case infinite loop.
  *
-*/
-#include <math.h>
-#include "edtaa3func.h"
+ * Updated 2012 to change license from LGPL to MIT.
+ *
+ * Updated 2014 to fix a bug with the 'gy' gradient computation.
+ *
+ */
+
+/*
+ Copyright (C) 2009-2012 Stefan Gustavson (stefan.gustavson@gmail.com)
+ The code in this file is distributed under the MIT license:
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
 /*
  * Compute the local gradient at edge pixels using convolution filters.
@@ -65,15 +61,15 @@
  */
 void computegradient(double *img, int w, int h, double *gx, double *gy)
 {
-    int i,j,k;
-    double glength;
+    int i,j,k,p,q;
+    double glength, phi, phiscaled, ascaled, errsign, pfrac, qfrac, err0, err1, err;
 #define SQRT2 1.4142136
     for(i = 1; i < h-1; i++) { // Avoid edges where the kernels would spill over
         for(j = 1; j < w-1; j++) {
             k = i*w + j;
             if((img[k]>0.0) && (img[k]<1.0)) { // Compute gradient for edge pixels only
                 gx[k] = -img[k-w-1] - SQRT2*img[k-1] - img[k+w-1] + img[k-w+1] + SQRT2*img[k+1] + img[k+w+1];
-                gy[k] = -img[k-w-1] - SQRT2*img[k-w] - img[k+w-1] + img[k-w+1] + SQRT2*img[k+w] + img[k+w+1];
+                gy[k] = -img[k-w-1] - SQRT2*img[k-w] - img[k-w+1] + img[k+w-1] + SQRT2*img[k+w] + img[k+w+1];
                 glength = gx[k]*gx[k] + gy[k]*gy[k];
                 if(glength > 0.0) { // Avoid division by zero
                     glength = sqrt(glength);
