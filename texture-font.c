40c8c14b8b6b2678a9e7ec73c37fec71caa81eef
DarkShinz <shin_ninja@hotmail.com>
2018-08-03 16:36:25 -0300

    Aligned blocks  to keep coding style

--- texture-font.c
+++ texture-font.c
@@ -572,12 +572,13 @@ cleanup_stroker:
         padding.left = 1;
     }
 	
-	if (self->padding != 0) {
-		padding.top += self->padding;
-		padding.left += self->padding;
-		padding.right += self->padding;
-		padding.bottom += self->padding;
-	}
+    if (self->padding != 0)
+    {
+	padding.top += self->padding;
+	padding.left += self->padding;
+	padding.right += self->padding;
+	padding.bottom += self->padding;
+    }
 
     size_t src_w = ft_bitmap.width/self->atlas->depth;
     size_t src_h = ft_bitmap.rows;
