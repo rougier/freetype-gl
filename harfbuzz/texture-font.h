d79ceedeba72a4c6b97d9d82228dc790797572a7
mikeiannacone <mike.iannacone@gmail.com>
2021-06-29 17:38:20 -0400

    remove redundant rendermode_t def from texture-font.h

--- harfbuzz/texture-font.h
+++ harfbuzz/texture-font.h
@@ -58,19 +58,6 @@ typedef enum rendermode_t
     RENDER_SIGNED_DISTANCE_FIELD
 } rendermode_t;
 
-
-/**
- * A list of possible ways to render a glyph.
- */
-typedef enum rendermode_t
-{
-    RENDER_NORMAL,
-    RENDER_OUTLINE_EDGE,
-    RENDER_OUTLINE_POSITIVE,
-    RENDER_OUTLINE_NEGATIVE,
-    RENDER_SIGNED_DISTANCE_FIELD
-} rendermode_t;
-
 /**
  * A structure that describe a glyph.
  */
