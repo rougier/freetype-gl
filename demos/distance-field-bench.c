16753224a1e48242ae023d488da2352fb792301e
duvisit <ivnrdc@gmail.com>
2024-01-27 11:54:39 +0100

    Add freetype-gl banner

--- demos/distance-field-bench.c
+++ demos/distance-field-bench.c
@@ -1,3 +1,9 @@
+/* Freetype GL - A C OpenGL Freetype engine
+ *
+ * Distributed under the OSI-approved BSD 2-Clause License.  See accompanying
+ * file `LICENSE` for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
